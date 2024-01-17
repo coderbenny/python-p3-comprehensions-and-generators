d00952014e244bfdcbda66a69e3456b2d0ba2cc7
bennyhinnmathew <benny.mathew@student.moringaschool.com>
2024-01-17 07:42:09 +0300

    Completed Lab

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,23 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_list = [n for n in num_list if n % 2 == 0]
+    if len(even_list) == 0:
+        print([])
+        return []
+    else:
+        print(even_list)
+        return even_list
+    
+return_evens([1,25])
 
 def make_exclamation(sentence_list):
-    pass
+    final_list = [n + "!" for n in sentence_list]
+    if final_list:
+        print(final_list)
+        return final_list
+    else:
+        print([])
+        return []
+
+make_exclamation(["Hello", "I am doing great","Python is fun"])
